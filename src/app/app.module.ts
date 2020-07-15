7e2798076f030a91047962cadb57a73d07536059
CiobanuMarian <49883613+CiobanuMarian@users.noreply.github.com>
2020-07-15 19:11:01 +0300

    login html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,55 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule} from '@Angular/fire/database';
+import { environment } from 'src/environments/environment';
+import { LoginComponent } from './login/login.component';
+import { MatTableModule, MatButtonModule, MatFormField, MatGridListModule, MatDatepickerModule, MatSelectModule, MatInputModule, MatNativeDateModule, MatIconModule, MatPaginatorModule, MatMenuModule, MatSortModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule, FormsModule, NgForm } from '@angular/forms';
+import {MatCardModule, MatCard} from '@angular/material/card';
 
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AppRoutingModule,
+    BrowserModule,
+    AppRoutingModule,
+    MatCardModule,
+    BrowserModule,
+    HttpClientModule,
+    MatTableModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatGridListModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatMenuModule,
+    RouterModule.forRoot(
+      routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+ 
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAmVLJRzHUryzyfZkfDH7yTH8RDBJb56kM",
+    authDomain: "angulardeploy-77e35.firebaseapp.com",
+    databaseURL: "https://angulardeploy-77e35.firebaseio.com",
+    projectId: "angulardeploy-77e35",
+    storageBucket: "angulardeploy-77e35.appspot.com",
+    messagingSenderId: "513795349804",
+    appId: "1:513795349804:web:a7d17b633290f231246089",
+    measurementId: "G-3225VT4LV1"
+  }
 };
